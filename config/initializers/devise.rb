980ef1cef4019499c0759c691600758b5551a14d
Tilda Udufo <mathildaudufo@gmail.com>
2020-11-16 14:40:34 +0100

    Updated facebook scopes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], scope: 'public_profile,email'
+  config.omniauth :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], { scope: 'email', info_fields: 'email, name' }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
