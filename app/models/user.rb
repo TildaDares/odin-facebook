93c71856ec36b2fa7ca4ae7c63398fb621bcbe73
Tilda Udufo <mathildaudufo@gmail.com>
2020-11-18 13:15:57 +0100

    added dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
-  has_many :comments
-  has_many :notifications
-  has_many :friendships
+  has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
   has_many :inverse_friends, through: :inverse_friendships, source: :user
   acts_as_favoritor
   after_create_commit :send_confirmation_email
