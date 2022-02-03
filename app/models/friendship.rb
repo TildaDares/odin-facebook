72c1401333b11de7839a3ae6b6023091a2562042
TildaDares <mathildaudufo@gmail.com>
2022-02-04 00:49:19 +0100

    minor changes

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,4 @@
 class Friendship < ApplicationRecord
-  belongs_to :user # request receiver
-  belongs_to :friend, class_name: 'User' # request sender
+  belongs_to :user # friend request sender
+  belongs_to :friend, class_name: 'User' # friend request receiver
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,31 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
+  has_many :searches, dependent: :destroy
+
+  # sending a request
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
-  has_many :searches, dependent: :destroy
+
+  # receiving a request
+  # inverse friends looks for a user that has my friend_id equal to my id
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
   has_many :inverse_friends, through: :inverse_friendships, source: :user
+
   acts_as_favoritor
   after_create_commit :send_confirmation_email
   has_one_attached :avatar
   has_one_attached :header
+
   validates :username, presence: true, length: { minimum: 1, maximum: 50 }
   validates :bio, length: { maximum: 160 }
   validates :location, length: { maximum: 30 }
   validates :avatar, content_type: [:png, :jpg, :jpeg]
   validates :header, content_type: [:png, :jpg, :jpeg]
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
